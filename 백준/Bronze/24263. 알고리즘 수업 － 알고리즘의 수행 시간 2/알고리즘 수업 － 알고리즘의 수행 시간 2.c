960aeaaaa272ca37b37b5886bc7ab6151379c867
Eden <152848222+eden3595@users.noreply.github.com>
2024-12-26 17:44:15 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    // 첫 번째 줄: 코드1의 수행 횟수
+    printf("%d\n", n);
+
+    // 두 번째 줄: 다항식의 최고차항 차수
+    printf("1\n");
+
+    return 0;
+}
