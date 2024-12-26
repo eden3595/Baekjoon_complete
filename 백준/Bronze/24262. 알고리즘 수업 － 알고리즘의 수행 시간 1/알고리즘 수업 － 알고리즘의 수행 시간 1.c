c6f08e478130205f973056fd49c42b27baf64b1a
Eden <152848222+eden3595@users.noreply.github.com>
2024-12-26 17:36:51 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+int main() {
+
+	unsigned int n;
+	scanf("%u", &n);
+
+	printf("1\n");
+
+	printf("0");
+	
+	return 0;
+}
