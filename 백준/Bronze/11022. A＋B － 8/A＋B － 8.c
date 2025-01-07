4be770405bc95c84c6b105382ff1ce463165595e
Eden <152848222+eden3595@users.noreply.github.com>
2025-01-07 14:05:14 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	int cnt = 0;
+
+	while (1) {
+
+		if (cnt == n) {
+			break;
+		}
+
+		else {
+
+			int a, b;
+			scanf("%d %d", &a, &b);
+
+			printf("Case #%d: %d + %d = %d\n", cnt + 1,a,b, a + b);
+			cnt++;
+		}
+
+	}
+
+	return 0;
+}
