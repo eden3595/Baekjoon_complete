4539620b8cc0de4106876ecf9b437131c7c871d0
Eden <152848222+eden3595@users.noreply.github.com>
2025-01-07 13:27:11 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--) {
+		printf("%d\n",  i);
+	}
+
+	return 0;
+}
