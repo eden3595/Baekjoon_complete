7815af506818c90d7045fd55b9ad303872a0f851
Eden <152848222+eden3595@users.noreply.github.com>
2025-01-07 13:28:25 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
